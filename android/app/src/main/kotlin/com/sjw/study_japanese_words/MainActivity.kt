e15ba7a2079f0934c87aeff9a0824afc9d1950ef
eckim <eckim@amtel.co.kr>
2024-03-05 09:15:23 +0900

    make go_router file

---
+++ android/app/src/main/kotlin/com/sjw/study_japanese_words/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sjw.study_japanese_words
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(_App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
