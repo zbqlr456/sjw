26612dd43b4a09948e53d7c5c62164edcac855ad
eckim <eckim@amtel.co.kr>
2024-03-25 16:43:08 +0900

    크롤링 완죠, asset 추가

--- crawling.py
+++ crawling.py
@@ -2,14 +2,12 @@ import io
 import sys
 import time
 import pandas as pd
+import openpyxl
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-# .row > .origin > .text 히라가나
-# .row > .origin > .pronunciation 한자
-# .row > .mean_list > .mean_item > .mean 뜻
 # https://ja.dict.naver.com/#/jlpt/list?level=1&part=noun&page=1
 # level 1 ~ 5
 # page 1 ~ 끝까지 
@@ -21,28 +19,47 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 driver = webdriver.Chrome()
 clist = ['noun', 'verb', 'adjective', 'adverb', 'measurement', 'phrases', 'conjunction', 'numeral', 'pronoun', 'interjection']
 
+filename = "japanese_crawling.xlsx"
 data = []
+                
+def append_to_excel(filename, data):
+    try:
+        wb = openpyxl.load_workbook(filename)
+    except FileNotFoundError:
+        wb = openpyxl.Workbook()
+        wb.create_sheet(index=0, title="Sheet1")
+    sheet = wb["Sheet1"]
+    
+    sheet.append(data)
+    
+    wb.save(filename)
 
 for level in range(1, 6):
     for word_class in clist:
         url = f'https://ja.dict.naver.com/#/jlpt/list?level={level}&part={word_class}&page=1'
         driver.get(url)
-        time.sleep(2)
+        time.sleep(1)
         amount = driver.find_element(By.CLASS_NAME, 'amount').text
         page_num = int(int(amount.replace(',','').replace('건','')) / 10 + 2)
 
         for page in range(1, page_num):
             url = f'https://ja.dict.naver.com/#/jlpt/list?level={level}&part={word_class}&page={page}'
             driver.get(url)
-            time.sleep(3)
+            time.sleep(1)
 
             words = driver.find_elements(By.CLASS_NAME, 'row')
             for word in words:
-
-                row = word.text.split('\n') # \n 을 기준으로 3개로 나눔.
                 chinese = ''
                 japanese = ''
                 korean = ''
+                
+                row = word.text.split('\n') # \n 을 기준으로 3개로 나눔.
+                
+                # row[0]: 'あつりょく [圧力]' --> [이 있으면 [] 안의 문자는 한자, [의 이전 문자들은 히라가나. [이 없으면 전부다 히라가나, 한자는 x
+                # row[1]: '듣기' --> 사용 안함
+                # row[2]: '명사 압력' --> 명사 이후의 문자열 그대로 뜻.
+                
+                
                 if '[' in row[0]:
                     parts = row[0].split('[')
                     japanese = parts[0].strip()
@@ -50,19 +67,8 @@ for level in range(1, 6):
                 else:
                     japanese = row[0].strip()
                 korean = row[2][3:]
-                    
-                print(chinese)
-                print(japanese)
-                print(korean)
+                
+                # data.append([chinese, japanese, korean])
+                print([chinese, japanese, korean])
+                append_to_excel(filename, [chinese, japanese, korean])
 
-                # あつりょく [圧力] --> [이 있으면 [] 안의 문자는 한자, [의 이전 문자들은 히라가나. [이 없으면 전부다 히라가나, 한자는 x
-                # 듣기 --> 
-                # 명사 압력 --> 명사 이후의 문자열 그대로 뜻.
-                print('-----------------------------------')
-                # data.append(word.text)
-
-
-# df = pd.DataFrame(data, colums = ['Word'])
-# df.to_excel('words.xlsx', index = False, engine = 'openpyxl')
-
-# driver.quit()
